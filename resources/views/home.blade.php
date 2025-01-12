0d768e248c02de8d93851325e3e85912612c7813
Muhamad Iqbal fauzi <miqbalf2021@gmail.com>
2025-01-12 12:13:21 +0700

    perbaikan responsive design

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -35,10 +35,17 @@
 </section>
 
 
-<section id="moto" class="container mx-auto py-16 bg-emerald-500 relative">
+<section id="moto" class="container mx-auto ">
     <!-- Wave Border Section -->
+    <div class="w-full h-20 hidden md:block">
+        <svg viewBox="0 0 1440 100" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path
+                d="M0 43.9999C106.667 43.9999 213.333 7.99994 320 7.99994C426.667 7.99994 533.333 43.9999 640 43.9999C746.667 43.9999 853.333 7.99994 960 7.99994C1066.67 7.99994 1173.33 43.9999 1280 43.9999C1386.67 43.9999 1440 19.0266 1440 9.01329V100H0V43.9999Z"
+                class="fill-current text-emerald-500"></path>
+        </svg>
+    </div>
 
-
+    <div class="pb-20 pt-10 bg-emerald-500 relative">
     <h2 class="text-3xl font-bold text-center text-orange-500">Moto</h2>
 
     <div class=" mb-12 mx-4 sm:mx-8 lg:mx-12">
@@ -69,6 +76,7 @@
             <p class="text-gray-700">Mengasah keterampilan praktis untuk menjadi individu yang siap menghadapi tantangan dunia.</p>
         </div>
     </div>
+</div>
 </section>
 
     
